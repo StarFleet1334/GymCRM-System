8f4371b8352258c7f3fbf0055e3c59e20da2e50b
IliaLataria01 <latariailia6@gmail.com>
2024-09-03 13:26:53 +0400

    update .gradle

--- src/test/java/com/demo/folder/service/TrainingServiceTest.java
+++ src/test/java/com/demo/folder/service/TrainingServiceTest.java
@@ -12,7 +12,6 @@ import java.util.Date;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
